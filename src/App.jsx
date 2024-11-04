9703f130f0af98d921077e15b44469df963f8ec1
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-04 11:03:00 +0530

    created react-hook-form app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useForm } from 'react-hook-form'
 import './App.css'
 
 function App() {
-
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  
   return (
     <>
+    <div>
+      Hello 
+    </div>
     </>
   )
 }
