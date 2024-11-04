199d2375a45eb9fa3d66723a6bd77c0e2e6e8541
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-04 11:22:16 +0530

    basic set of forms using react-hook-forms

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,31 @@ function App() {
     watch,
     formState: { errors },
   } = useForm();
-  
+
+  function onSubmit(data){
+    console.log('Submiting the form', data);
+  }
+
   return (
     <>
+    <form onSubmit={handleSubmit(onSubmit)}>
     <div>
-      Hello 
+      <label> First Name: </label>
+      <input {...register('firstName')}/>
     </div>
+    <br/>
+    <div>
+      <label> Middle Name: </label>
+      <input {...register('midName')}/>
+    </div>
+    <br/>
+    <div>
+      <label> Last Name: </label>
+      <input {...register('lastName')}/>
+    </div>
+    <br/>
+    <input type='submit'/>
+    </form>
     </>
   )
 }
